b03feb9ffc7711bcf1f7680b0d3ca6c21ea374bc
Sakaay <56340246+sa-da-sa-da@users.noreply.github.com>
2025-12-14 22:45:44 +0800

    Update WelcomeCard.vue

--- docs/.vitepress/ConfigHyde/Wallaper.ts
+++ docs/.vitepress/ConfigHyde/Wallaper.ts
@@ -8,7 +8,6 @@ export const Wallpaper = [
   "http://cdn-hsyq-static.shanhutech.cn/bizhi/staticwp/202305/06e8a51ef12581befd70b168dd914d77--3968391448.jpg",
   "https://cdn-hsyq-static-bak.shanhutech.cn/a/201908/22/5d5e7c1591d27.jpg",
   "https://cdn-hsyq-static-bak.shanhutech.cn/bizhi/staticwp/201704/1555298b8bcf9421b5fb2cd96a9fc4d5.jpg",
-  "https://cdn-hsyq-static-bak.shanhutech.cn/bizhi/staticwp/201803/6834170cf8d094e81d7701a7c8b7f358.jpgp",
   "https://cdn-hsyq-static-bak.shanhutech.cn/bizhi/staticwp/201704/e683f9459eaf7d58ab8c8bd2bbf82821.jpg",
   "https://cdn-hsyq-static-bak.shanhutech.cn/bizhi/staticwp/201711/f5d336a399accb02f8d5dcd081638f67.jpg",
  
